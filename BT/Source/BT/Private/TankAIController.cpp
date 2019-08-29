fa279deaf1ade2fefb53151ee6180cb7d591d8e4
Hassan Wansa <htw961@gmail.com>
2019-08-29 02:58:08 -0700

    Added AIController and had it find the player also have the player announce if playercontroller is controlling it

---
+++ BT/Source/BT/Private/TankAIController.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController doesn't see Player Tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *(PlayerTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	APlayerController* PlayerPC = GetWorld()->GetFirstPlayerController();
+
+	return Cast<ATank>(PlayerPC->GetPawn());
+}
