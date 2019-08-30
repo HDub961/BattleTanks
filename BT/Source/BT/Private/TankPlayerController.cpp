9284de7a4cdf723fc6317f04600dd40f8e3fbe04
Hassan Wansa <htw961@gmail.com>
2019-08-30 00:49:10 -0700

    Fixed up tank player controller to get world location from screen location

--- BT/Source/BT/Private/TankPlayerController.cpp
+++ BT/Source/BT/Private/TankPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "TankPlayerController.h"
 
+#define OUT
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -18,7 +19,52 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; // Out parameter
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *HitLocation.ToString());
+		// TODO Tell controlled tank to aim at this point
+	}
+
+}
+
+// Get world location if linetrace through crosshair, true if it hits the landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutLocation) const
+{
+	// Find the crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+
+	// "De-project" the screen position of the crosshair to a world direction
+	FVector LookDirection;
+	if(GetLookDirection(ScreenLocation, LookDirection))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *LookDirection.ToString());
+	}
+
+	// Line-trace along that look direction, and see what we hit (up to max range)
+	return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector WorldLocation;// For discarding for now
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLocation, LookDirection);
 }
--- BT/Source/BT/Public/TankPlayerController.h
+++ BT/Source/BT/Public/TankPlayerController.h
@@ -16,7 +16,24 @@ class BT_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
+
+	virtual void Tick( float DeltaTime ) override;
+
+private:
+
+	ATank* GetControlledTank() const;
+
+	bool GetSightRayHitLocation(FVector& OutLocation) const;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector & LookDirection) const;
+	
+	void AimTowardsCrosshair();
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.33333;
 };
