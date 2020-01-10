70629d20aa896f8e999a334fcdb965b5be2e8e33
Hassan Wansa <htw961@gmail.com>
2020-01-10 06:01:42 -0800

    Fixed up turret rotation

--- BT/Source/BT/Public/Tank.h
+++ BT/Source/BT/Public/Tank.h
@@ -7,7 +7,8 @@
 #include "Tank.generated.h"
 
 // Forward Declaration
-class UTankBarrel; 
+class UTankBarrel;
+class UTankTurret;
 class UTankAimingComponent;
 
 UCLASS()
@@ -21,6 +22,9 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+		void SetTurretReference(UTankTurret* TurretToSet);
 
 protected:
 
@@ -37,6 +41,6 @@ private:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	UPROPERTY(EditAnywhere, Category = Firing)
-	float LaunchSpeed = 10000; // TODO find sensible default
+	float LaunchSpeed = 4000;
 
 };
