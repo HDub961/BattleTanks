330eab581b1b5cd7470da7ea793e7bd870fd2947
Hassan Wansa <htw961@gmail.com>
2020-01-29 05:21:30 -0800

    Setting up projectile

--- BT/Source/BT/Private/TankAimingComponent.cpp
+++ BT/Source/BT/Private/TankAimingComponent.cpp
@@ -51,14 +51,7 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	if(bHaveAimSolution)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
-		MoveBarrelTowards(AimDirection);
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: aim solution found"), Time);
-	}
-	else
-	{
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: no aim solution found"), Time);
+		MoveBarrelTowards(AimDirection);	
 	}
 }
 
